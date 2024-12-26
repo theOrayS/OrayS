arceos-org/arceos

[config] Refactor config system with axconfig-gen

modules/axhal/src/platform/riscv64_qemu_virt/boot.rs
@@ -1,6 +1,6 @@
 use riscv::register::satp;
 
-use axconfig::{PHYS_VIRT_OFFSET, TASK_STACK_SIZE};
+use axconfig::{TASK_STACK_SIZE, platform::PHYS_VIRT_OFFSET};
 
 #[unsafe(link_section = ".bss.stack")]
 static mut BOOT_STACK: [u8; TASK_STACK_SIZE] = [0; TASK_STACK_SIZE];

modules/axmm/src/lib.rs
@@ -33,8 +33,8 @@ fn paging_err_to_ax_err(err: PagingError) -> AxError {
 /// Creates a new address space for kernel itself.
 pub fn new_kernel_aspace() -> AxResult<AddrSpace> {
     let mut aspace = AddrSpace::new_empty(
-        va!(axconfig::KERNEL_ASPACE_BASE),
-        axconfig::KERNEL_ASPACE_SIZE,
+        va!(axconfig::platform::KERNEL_ASPACE_BASE),
+        axconfig::platform::KERNEL_ASPACE_SIZE,
     )?;
     for r in axhal::mem::memory_regions() {
         aspace.map_linear(phys_to_virt(r.paddr), r.paddr, r.size, r.flags.into())?;

theOrayS/OrayS

modules/axhal/src/platform/riscv64_qemu_virt/boot.rs
@@ -1,6 +1,6 @@
 use riscv::register::satp;
 
-use axconfig::{PHYS_VIRT_OFFSET, TASK_STACK_SIZE};
+use axconfig::{TASK_STACK_SIZE, plat::PHYS_VIRT_OFFSET};
 
 #[unsafe(link_section = ".bss.stack")]
 static mut BOOT_STACK: [u8; TASK_STACK_SIZE] = [0; TASK_STACK_SIZE];

modules/axmm/src/lib.rs
@@ -33,8 +33,8 @@ fn paging_err_to_ax_err(err: PagingError) -> AxError {
 /// Creates a new address space for kernel itself.
 pub fn new_kernel_aspace() -> AxResult<AddrSpace> {
     let mut aspace = AddrSpace::new_empty(
-        va!(axconfig::KERNEL_ASPACE_BASE),
-        axconfig::KERNEL_ASPACE_SIZE,
+        va!(axconfig::plat::KERNEL_ASPACE_BASE),
+        axconfig::plat::KERNEL_ASPACE_SIZE,
     )?;
     for r in axhal::mem::memory_regions() {
         aspace.map_linear(phys_to_virt(r.paddr), r.paddr, r.size, r.flags.into())?;