jizhaoqin/arceos

[hal] replace axhal::arch with crate axcpu

modules/axhal/src/platform/aarch64_phytium_pi/mod.rs
@@ -20,7 +20,7 @@ pub mod misc {
     pub fn terminate() -> ! {
         info!("Shutting down...");
         loop {
-            crate::arch::halt();
+            axcpu::asm::halt();
         }
     }
 }
@@ -34,7 +34,7 @@ unsafe extern "C" {
 pub(crate) unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
     crate::mem::clear_bss();
     let cpu_id = cpu_hard_id_to_logic_id(cpu_id);
-    crate::arch::write_page_table_root0(0.into()); // disable low address access
+    axcpu::init::init_cpu();
     crate::cpu::init_primary(cpu_id);
     super::aarch64_common::pl011::init_early();
     super::aarch64_common::generic_timer::init_early();
@@ -44,7 +44,7 @@ pub(crate) unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
 #[cfg(feature = "smp")]
 pub(crate) unsafe extern "C" fn rust_entry_secondary(cpu_id: usize) {
     let cpu_id = cpu_hard_id_to_logic_id(cpu_id);
-    crate::arch::write_page_table_root0(0.into()); // disable low address access
+    axcpu::init::init_cpu();
     crate::cpu::init_secondary(cpu_id);
     rust_main_secondary(cpu_id);
 }

modules/axhal/src/platform/aarch64_qemu_virt/mod.rs
@@ -28,6 +28,7 @@ unsafe extern "C" {
 
 pub(crate) unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
     crate::mem::clear_bss();
+    axcpu::init::init_cpu();
     crate::cpu::init_primary(cpu_id);
     super::aarch64_common::pl011::init_early();
     super::aarch64_common::generic_timer::init_early();
@@ -36,6 +37,7 @@ pub(crate) unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
 
 #[cfg(feature = "smp")]
 pub(crate) unsafe extern "C" fn rust_entry_secondary(cpu_id: usize) {
+    axcpu::init::init_cpu();
     crate::cpu::init_secondary(cpu_id);
     rust_main_secondary(cpu_id);
 }

modules/axhal/src/platform/loongarch64_qemu_virt/mod.rs
@@ -27,6 +27,7 @@ unsafe extern "C" {
 /// This function will be called after assembly boot stage.
 unsafe extern "C" fn rust_entry(cpu_id: usize) {
     crate::mem::clear_bss();
+    axcpu::init::init_cpu();
     super::console::init_early();
     crate::cpu::init_primary(cpu_id);
     super::time::init_primary();
@@ -40,6 +41,7 @@ unsafe extern "C" fn rust_entry(cpu_id: usize) {
 #[cfg(feature = "smp")]
 /// The entry point for the second core.
 pub(crate) extern "C" fn rust_entry_secondary(cpu_id: usize) {
+    axcpu::init::init_cpu();
     crate::cpu::init_secondary(cpu_id);
     super::time::init_percpu();
 

modules/axhal/src/platform/riscv64_qemu_virt/mod.rs
@@ -19,6 +19,7 @@ unsafe extern "C" {
 
 unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
     crate::mem::clear_bss();
+    axcpu::init::init_cpu();
     crate::cpu::init_primary(cpu_id);
     self::time::init_early();
     rust_main(cpu_id, dtb);
@@ -26,6 +27,7 @@ unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
 
 #[cfg(feature = "smp")]
 unsafe extern "C" fn rust_entry_secondary(cpu_id: usize) {
+    axcpu::init::init_cpu();
     crate::cpu::init_secondary(cpu_id);
     rust_main_secondary(cpu_id);
 }

theOrayS/OrayS

modules/axhal/src/platform/aarch64_phytium_pi/mod.rs
@@ -20,7 +20,7 @@ pub mod misc {
     pub fn terminate() -> ! {
         info!("Shutting down...");
         loop {
-            crate::arch::halt();
+            axcpu::asm::halt();
         }
     }
 }
@@ -34,7 +34,7 @@ unsafe extern "C" {
 pub(crate) unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
     crate::mem::clear_bss();
     let cpu_id = cpu_hard_id_to_logic_id(cpu_id);
-    crate::arch::write_page_table_root0(0.into()); // disable low address access
+    axcpu::init::init_trap();
     crate::cpu::init_primary(cpu_id);
     super::aarch64_common::pl011::init_early();
     super::aarch64_common::generic_timer::init_early();
@@ -44,7 +44,7 @@ pub(crate) unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
 #[cfg(feature = "smp")]
 pub(crate) unsafe extern "C" fn rust_entry_secondary(cpu_id: usize) {
     let cpu_id = cpu_hard_id_to_logic_id(cpu_id);
-    crate::arch::write_page_table_root0(0.into()); // disable low address access
+    axcpu::init::init_trap();
     crate::cpu::init_secondary(cpu_id);
     rust_main_secondary(cpu_id);
 }

modules/axhal/src/platform/aarch64_qemu_virt/mod.rs
@@ -28,6 +28,7 @@ unsafe extern "C" {
 
 pub(crate) unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
     crate::mem::clear_bss();
+    axcpu::init::init_trap();
     crate::cpu::init_primary(cpu_id);
     super::aarch64_common::pl011::init_early();
     super::aarch64_common::generic_timer::init_early();
@@ -36,6 +37,7 @@ pub(crate) unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
 
 #[cfg(feature = "smp")]
 pub(crate) unsafe extern "C" fn rust_entry_secondary(cpu_id: usize) {
+    axcpu::init::init_trap();
     crate::cpu::init_secondary(cpu_id);
     rust_main_secondary(cpu_id);
 }

modules/axhal/src/platform/loongarch64_qemu_virt/mod.rs
@@ -27,6 +27,7 @@ unsafe extern "C" {
 /// This function will be called after assembly boot stage.
 unsafe extern "C" fn rust_entry(cpu_id: usize) {
     crate::mem::clear_bss();
+    axcpu::init::init_trap();
     super::console::init_early();
     crate::cpu::init_primary(cpu_id);
     super::time::init_primary();
@@ -40,6 +41,7 @@ unsafe extern "C" fn rust_entry(cpu_id: usize) {
 #[cfg(feature = "smp")]
 /// The entry point for the second core.
 pub(crate) extern "C" fn rust_entry_secondary(cpu_id: usize) {
+    axcpu::init::init_trap();
     crate::cpu::init_secondary(cpu_id);
     super::time::init_percpu();
 

modules/axhal/src/platform/riscv64_qemu_virt/mod.rs
@@ -19,6 +19,7 @@ unsafe extern "C" {
 
 unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
     crate::mem::clear_bss();
+    axcpu::init::init_trap();
     crate::cpu::init_primary(cpu_id);
     self::time::init_early();
     rust_main(cpu_id, dtb);
@@ -26,6 +27,7 @@ unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
 
 #[cfg(feature = "smp")]
 unsafe extern "C" fn rust_entry_secondary(cpu_id: usize) {
+    axcpu::init::init_trap();
     crate::cpu::init_secondary(cpu_id);
     rust_main_secondary(cpu_id);
 }