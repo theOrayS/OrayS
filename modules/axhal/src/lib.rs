40ceab3200fa8a87041b85d305667c35c8b782cd
Su Mingxian <aarkegz@gmail.com>
2025-12-19 14:37:54 +0800

    Introduce max-cpu-num config item, remove all direct usages of axconfig::plat::CPU_NUM, use axplat to get cpu num. (#317)
    * squash from 28dc592 to 4f805fb
    
    * update axplat_crates for arceos-helloworld-myplat
    
    * fix errors after cherry-picking
    
    * add `smp = {}` output for test purpose
    
    * fix an out-dated comment
    
    * move `init_cpu_num` into `init_later`
    
    * cache `cpu_num` in select_run_queue_index

--- modules/axhal/src/lib.rs
+++ modules/axhal/src/lib.rs
@@ -36,6 +36,10 @@ extern crate log;
 
 #[allow(unused_imports)]
 #[macro_use]
+extern crate axlog;
+
+#[allow(unused_imports)]
+#[macro_use]
 extern crate memory_addr;
 
 cfg_if::cfg_if! {
@@ -103,10 +107,11 @@ pub mod context {
 }
 
 pub use axcpu::asm;
-pub use axplat::init::init_later;
 
 #[cfg(feature = "smp")]
 pub use axplat::init::{init_early_secondary, init_later_secondary};
+#[cfg(feature = "smp")]
+use core::sync::atomic::{AtomicUsize, Ordering};
 
 /// Initializes CPU-local data structures for the primary core.
 ///
@@ -125,10 +130,6 @@ pub fn init_percpu_secondary(cpu_id: usize) {
     self::percpu::init_secondary(cpu_id);
 }
 
-use lazyinit::LazyInit;
-
-static BOOT_ARG: LazyInit<usize> = LazyInit::new();
-
 /// Initializes the platform and boot argument.
 /// This function should be called as early as possible.
 pub fn init_early(cpu_id: usize, arg: usize) {
@@ -136,8 +137,78 @@ pub fn init_early(cpu_id: usize, arg: usize) {
     axplat::init::init_early(cpu_id, arg);
 }
 
+/// Initializes the platform later stage.
+pub fn init_later(cpu_id: usize, arg: usize) {
+    axplat::init::init_later(cpu_id, arg);
+    init_cpu_num();
+}
+
+use lazyinit::LazyInit;
+
+static BOOT_ARG: LazyInit<usize> = LazyInit::new();
+
 /// Returns the boot argument.
 /// This is typically the device tree blob address passed from the bootloader.
 pub fn get_bootarg() -> usize {
     *BOOT_ARG
+}
+
+/// The number of CPUs in the system. Based on the number declared by the
+/// platform crate and limited by the configured maximum CPU number.
+#[cfg(feature = "smp")]
+static CPU_NUM: AtomicUsize = AtomicUsize::new(1);
+
+/// Gets the number of CPUs running in the system.
+///
+/// When SMP is disabled, this function always returns 1.
+///
+/// When SMP is enabled, it's the smaller one between the platform-declared CPU
+/// number [`axplat::power::cpu_num`] and the configured maximum CPU number
+/// `axconfig::plat::MAX_CPU_NUM`.
+///
+/// This value is determined during the BSP initialization phase.
+pub fn cpu_num() -> usize {
+    #[cfg(feature = "smp")]
+    {
+        // The BSP will always see the correct value because `CPU_NUM` is set by
+        // itself.
+        //
+        // All APs will see the correct value because it is written with
+        // `Ordering::Release` and read with `Ordering::Acquire`, ensuring
+        // memory visibility.
+        //
+        // Acquire may result in a performance penalty, but this function is not
+        // expected to be called frequently in normal operation.
+        CPU_NUM.load(Ordering::Acquire)
+    }
+    #[cfg(not(feature = "smp"))]
+    {
+        1
+    }
+}
+
+/// Initializes the CPU number information.
+fn init_cpu_num() {
+    #[cfg(feature = "smp")]
+    {
+        let plat_cpu_num = axplat::power::cpu_num();
+        let max_cpu_num = axconfig::plat::MAX_CPU_NUM;
+        let cpu_num = plat_cpu_num.min(max_cpu_num);
+
+        info!("CPU number: max = {max_cpu_num}, platform = {plat_cpu_num}, use = {cpu_num}",);
+        ax_println!("smp = {}", cpu_num); // for test purposes
+
+        if plat_cpu_num > max_cpu_num {
+            warn!(
+                "platform declares more CPUs ({plat_cpu_num}) than configured max ({max_cpu_num}), \
+                only the first {max_cpu_num} CPUs will be used."
+            );
+        }
+
+        CPU_NUM.store(cpu_num, Ordering::Release);
+    }
+    #[cfg(not(feature = "smp"))]
+    {
+        ax_println!("smp = 1"); // for test purposes
+    }
 }
