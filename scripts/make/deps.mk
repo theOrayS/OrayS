1072d8af5d1d9814e87b6fc69efa6b2ed01a7798
wangzhengzhuo <15397532686wzz@gmail.com>
2026-05-14 01:40:30 +0000

    fix: Update build.rs and deps.mk, trying to make remote judge machine work peoperly

--- scripts/make/deps.mk
+++ scripts/make/deps.mk
@@ -3,17 +3,17 @@
 # Tool to parse information about the target package
 ifeq ($(shell cargo axplat --version 2>/dev/null),)
   $(info Installing cargo-axplat...)
-  $(shell cargo install cargo-axplat)
+  $(shell cargo install --locked cargo-axplat)
 endif
 
 # Tool to generate platform configuration files
 ifeq ($(shell axconfig-gen --version 2>/dev/null),)
   $(info Installing axconfig-gen...)
-  $(shell cargo install axconfig-gen)
+  $(shell cargo install --locked axconfig-gen)
 endif
 
 # Cargo binutils
 ifeq ($(shell rust-objcopy --version 2>/dev/null),)
   $(info Installing cargo-binutils...)
-  $(shell cargo install cargo-binutils)
+  $(shell cargo install --locked cargo-binutils)
 endif
