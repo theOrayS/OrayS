e46ad1ebaf597c89c7550b80bd4cdc5a32cf502c
root <root@localhost.localdomain>
2026-05-17 06:13:53 +0800

    refactor: move pselect out of uspace

--- examples/shell/src/uspace/select_fdset.rs
+++ examples/shell/src/uspace/select_fdset.rs
@@ -1,7 +1,8 @@
 use axerrno::LinuxError;
 use linux_raw_sys::general;
 
-use super::linux_abi::{BITS_PER_USIZE, FD_SET_WORDS};
+use super::linux_abi::{BITS_PER_USIZE, FD_SET_WORDS, FD_SETSIZE, neg_errno};
+use super::signal_abi::current_unblocked_signal_pending;
 use super::user_memory::{read_user_value, write_user_value};
 use super::{FdTable, UserProcess};
 
@@ -80,4 +81,99 @@ pub(super) fn poll_fd_set(
         }
     }
     count
+}
+
+pub(super) fn sys_pselect6(
+    process: &UserProcess,
+    nfds: i32,
+    readfds: usize,
+    writefds: usize,
+    exceptfds: usize,
+    timeout: usize,
+    _sigmask: usize,
+) -> isize {
+    if nfds < 0 {
+        return neg_errno(LinuxError::EINVAL);
+    }
+    let nfds = (nfds as usize).min(FD_SETSIZE);
+    let read_bits = match read_fd_set(process, readfds) {
+        Ok(bits) => bits,
+        Err(err) => return neg_errno(err),
+    };
+    let write_bits = match read_fd_set(process, writefds) {
+        Ok(bits) => bits,
+        Err(err) => return neg_errno(err),
+    };
+    let except_bits = match read_fd_set(process, exceptfds) {
+        Ok(bits) => bits,
+        Err(err) => return neg_errno(err),
+    };
+    let deadline = match read_pselect_deadline(process, timeout) {
+        Ok(deadline) => deadline,
+        Err(err) => return neg_errno(err),
+    };
+    loop {
+        if current_unblocked_signal_pending() {
+            return neg_errno(LinuxError::EINTR);
+        }
+        let mut ready_read = [0usize; FD_SET_WORDS];
+        let mut ready_write = [0usize; FD_SET_WORDS];
+        let mut ready_except = [0usize; FD_SET_WORDS];
+        let ready = {
+            let table = process.fds.lock();
+            let mut count = 0usize;
+            count += poll_fd_set(&table, nfds, &read_bits, &mut ready_read, SelectMode::Read);
+            count += poll_fd_set(
+                &table,
+                nfds,
+                &write_bits,
+                &mut ready_write,
+                SelectMode::Write,
+            );
+            count += poll_fd_set(
+                &table,
+                nfds,
+                &except_bits,
+                &mut ready_except,
+                SelectMode::Except,
+            );
+            count
+        };
+        if ready > 0 {
+            let ret = write_fd_set(process, readfds, &ready_read);
+            if ret != 0 {
+                return ret;
+            }
+            let ret = write_fd_set(process, writefds, &ready_write);
+            if ret != 0 {
+                return ret;
+            }
+            let ret = write_fd_set(process, exceptfds, &ready_except);
+            if ret != 0 {
+                return ret;
+            }
+            // In this cooperative single-core environment, a hot readiness loop
+            // can otherwise starve the peer process that would consume the event.
+            axtask::yield_now();
+            return ready as isize;
+        }
+        if deadline.is_some_and(|ddl| axhal::time::wall_time() >= ddl) {
+            axtask::yield_now();
+            let empty = [0; FD_SET_WORDS];
+            let ret = write_fd_set(process, readfds, &empty);
+            if ret != 0 {
+                return ret;
+            }
+            let ret = write_fd_set(process, writefds, &empty);
+            if ret != 0 {
+                return ret;
+            }
+            let ret = write_fd_set(process, exceptfds, &empty);
+            if ret != 0 {
+                return ret;
+            }
+            return 0;
+        }
+        axtask::yield_now();
+    }
 }
